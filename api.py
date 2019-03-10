f9bee62032f72c1c8b6ce5b8645aac06d9dfaba5
martinord <martino_cabana@hotmail.com>
2019-03-10 17:14:21 +0000

    Handle request (user agent)

--- api.py
+++ api.py
@@ -7,7 +7,14 @@ class API:
     def __call__(self, environ, start_response):
         request = Request(environ)
 
-        response = Response()
-        response.text = "Hello World!"
+        response = self.handle_request(request)
 
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        user_agent = request.environ.get("HTTP_USER_AGENT", "No User Agent Found")
+
+        response = Response()
+        response.text = f"Hello, my friend with this user agent: {user_agent}"
+
+        return response
